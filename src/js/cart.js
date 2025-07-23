f9019438021631d0dfe53702594d18294ab0ec82
SantiagoHrd <santiagohrd1@gmail.com>
2025-07-23 18:19:45 -0500

    total $ in the cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,20 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  let total = 0;
+
+  if (cartItems.length > 0) {
+    document.querySelector(".cart-footer").classList.remove("hide");
+
+    cartItems.forEach((item) => {
+      total += item.FinalPrice;
+      document.querySelector(".cart-total").innerHTML = `Total: $${total}`;
+    });
+  }
 }
 
 function cartItemTemplate(item) {
